0091c409665f103c9cdf744e8303836f0068be80
Brian Rice <bcrice17@gjail.com>
2025-10-20 18:32:14 -0400

    Added assets and updated lists

---
+++ assets/json/controller-list.ts
@@ -0,0 +1,7 @@
+export const CONTROLLERS = [
+  { controllerNo:1, name:"Controller 1", url:"http://controller-1.local", isOnline:true},
+  { controllerNo:2, name:"Controller 2", url:"http://controller-2.local", isOnline:false },
+  { controllerNo:3, name:"Controller 3", url:"http://controller-3.local", isOnline:true },
+  { controllerNo:4, name:"Controller 4", url:"http://controller-4.local", isOnline:true },
+  { controllerNo:5, name:"Controller 5", url:"http://controller-5.local", isOnline:false }
+]
