0e7b1991c0d32a6717177fa62aab31033d4fadc8
Brian Rice <bcrice17@gjail.com>
2025-10-20 18:25:40 -0400

    Initial powercontroller improved app

---
+++ src/app/controller/controller.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controller',
+  imports: [],
+  templateUrl: './controller.html',
+  styleUrl: './controller.css'
+})
+export class Controller {
+
+}
