b7bf937de677e578d5cf18a884a2f1a61b22749b
Brian Rice <bcrice17@gjail.com>
2025-10-20 19:02:33 -0400

    Added most of the imports, need to seperate out the controller html and put the channels under controller

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+// Angular Material modules
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule
+  ],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/controller/controller.component.ts
+++ src/app/controller/controller.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-controller',
   standalone: true,
-  imports: [NgStyle],
+  imports: [],
   templateUrl: './controller.component.html',
   styleUrls: ['./controller.component.css']
 })
