53630cb1403def6f823dc3b1bc43f89af0a6c288
dizowned <bcrice17@gmail.com>
2025-10-21 13:41:23 -0400

    Fixed up controller, next onto channel

--- src/app/components/controller/controller.component.ts
+++ src/app/components/controller/controller.component.ts
@@ -1,8 +1,5 @@
-import { NgStyle } from '@angular/common';
-import { MatAccordion } from "@angular/material/expansion";
-import {ChangeDetectionStrategy, Component, input, viewChild} from '@angular/core';
+import {Component, input}  from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -35,7 +32,7 @@ export class Controller {
   constructor(){
     const count = this.noChannels();
     for (let i = 0; i < count; i++) {
-      this.channels.push(new ChannelComponent(this.channelList()[i].name));
+      this.channels.push(new ChannelComponent());
         }
   }
 
@@ -55,6 +52,19 @@ export class Controller {
         this.turnOff()
       else
         this.turnOn();
+  }
+
+  toggleAllOn(){
+    for (let i = 0; i < this.channels.length; i++) {
+      this.channels[i].channelEnabled = true;
+    }
+
+  }
+
+  toggleAllOff(){
+    for (let i = 0; i < this.channels.length; i++) {
+      this.channels[i].channelEnabled = false;
+    }
   }
 
 }
